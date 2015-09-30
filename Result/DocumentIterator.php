b76a2a9f389c2e884817d4aa2065abfeb8f6b7fe
pmishev <pmishev@gmail.com>
2015-09-30 10:35:46 +0100

    Removed some unused code

--- Result/DocumentIterator.php
+++ Result/DocumentIterator.php
@@ -2,10 +2,6 @@
 
 namespace Sineflow\ElasticsearchBundle\Result;
 
-//use ONGR\ElasticsearchBundle\Document\DocumentInterface;
-//use ONGR\ElasticsearchBundle\DSL\Aggregation\AbstractAggregation;
-//use ONGR\ElasticsearchBundle\Result\Aggregation\AggregationIterator;
-//use ONGR\ElasticsearchBundle\Result\Suggestion\SuggestionIterator;
 use Sineflow\ElasticsearchBundle\DTO\IndicesAndTypesMetadataCollection;
 
 /**
@@ -22,16 +18,6 @@ class DocumentIterator extends AbstractResultsIterator
      * @var IndicesAndTypesMetadataCollection
      */
     private $typesMetadataCollection;
-
-//    /**
-//     * @var AggregationIterator
-//     */
-//    private $aggregations;
-//
-//    /**
-//     * @var SuggestionIterator
-//     */
-//    private $suggestions;
 
     /**
      * @var string
@@ -86,16 +72,6 @@ class DocumentIterator extends AbstractResultsIterator
         return $this->getConverter($rawData['_index'], $rawData['_type'])->convertToDocument($rawData);
     }
 
-//    /**
-//     * @param string $type
-//     *
-//     * @return mixed
-//     */
-//    protected function getMapByType($type)
-//    {
-//        return $this->bundlesMapping[$this->typesMapping[$type]];
-//    }
-
     /**
      * Returns count of records found by given query.
      *
@@ -105,45 +81,4 @@ class DocumentIterator extends AbstractResultsIterator
     {
         return $this->rawData['hits']['total'];
     }
-
-//    /**
-//     * Returns aggregations.
-//     *
-//     * @return AggregationIterator
-//     */
-//    public function getAggregations()
-//    {
-//        if (isset($this->rawData['aggregations'])) {
-//            $data = [];
-//
-//            foreach ($this->rawData['aggregations'] as $key => $value) {
-//                $realKey = substr($key, strlen(AbstractAggregation::PREFIX));
-//                $data[$realKey] = $value;
-//            }
-//
-//            unset($this->rawData['aggregations']);
-//            $this->aggregations = new AggregationIterator($data, $this->getConverter());
-//        } elseif ($this->aggregations === null) {
-//            $this->aggregations = new AggregationIterator([]);
-//        }
-//
-//        return $this->aggregations;
-//    }
-//
-//    /**
-//     * Returns suggestions.
-//     *
-//     * @return SuggestionIterator
-//     */
-//    public function getSuggestions()
-//    {
-//        if (isset($this->rawData['suggest'])) {
-//            $this->suggestions = new SuggestionIterator($this->rawData['suggest']);
-//
-//            // Clear memory.
-//            unset($this->rawData['suggest']);
-//        }
-//
-//        return $this->suggestions;
-//    }
 }
