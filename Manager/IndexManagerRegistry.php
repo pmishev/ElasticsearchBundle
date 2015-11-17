ebc0fbea40992a3390791028a1a254d08c60fbf6
pmishev <pmishev@gmail.com>
2015-11-17 14:18:26 +0000

    Added getByEntity() method in IndexManagerRegistry

--- Manager/IndexManagerRegistry.php
+++ Manager/IndexManagerRegistry.php
@@ -2,6 +2,9 @@
 
 namespace Sineflow\ElasticsearchBundle\Manager;
 
+use Sineflow\ElasticsearchBundle\Document\DocumentInterface;
+use Sineflow\ElasticsearchBundle\Mapping\DocumentLocator;
+use Sineflow\ElasticsearchBundle\Mapping\DocumentMetadataCollector;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -14,6 +17,28 @@ class IndexManagerRegistry implements ContainerAwareInterface
      * @var ContainerInterface
      */
     private $container;
+
+    /**
+     * @var DocumentLocator
+     */
+    private $documentLocator;
+
+    /**
+     * @var DocumentMetadataCollector
+     */
+    private $metadataCollector;
+
+    /**
+     * Constructor
+     *
+     * @param DocumentLocator           $documentLocator
+     * @param DocumentMetadataCollector $metadataCollector
+     */
+    public function __construct(DocumentLocator $documentLocator, DocumentMetadataCollector $metadataCollector)
+    {
+        $this->documentLocator = $documentLocator;
+        $this->metadataCollector = $metadataCollector;
+    }
 
     /**
      * Sets the Container.
@@ -39,5 +64,19 @@ class IndexManagerRegistry implements ContainerAwareInterface
         }
 
         return $this->container->get($serviceName);
+    }
+
+    /**
+     * Returns the index manager managing a given Elasticsearch entity
+     *
+     * @param DocumentInterface $entity
+     * @return IndexManager
+     */
+    public function getByEntity(DocumentInterface $entity)
+    {
+        $documentClass = $this->documentLocator->getShortClassName(get_class($entity));
+        $indexManagerName = $this->metadataCollector->getDocumentClassIndex($documentClass);
+
+        return $this->get($indexManagerName);
     }
 }
